a0c9209c1c13428c4c26d13bf13e40d55cd613bd
jannakam <jannakalmuqaisib@gmail.com>
2024-10-15 11:50:00 +0300

    Page 2

--- nextjs-shop/src/app/page.js
+++ nextjs-shop/src/app/page.js
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import ProductsList from './ProductsList.js'
 
 export default function Home() {
-  const shopName = <h1 className="text-4xl font-bold">Cookies and Beyond</h1>
-  const shopDescription = <p className="text-lg font-bold">Where cookie maniacs gather</p>
+  const shopName = <h1 className="text-4xl font-bold font-serif">Cookies and Beyond</h1>
+  const shopDescription = <p className="text-lg font-bold font-serif">Where cookie maniacs gather</p> 
+
   return (
     <div className="justify-center flex items-center flex-col space-y-6">
     {shopName}
@@ -13,6 +15,7 @@ export default function Home() {
     height={500}
     alt="Picture of my store"
     />
+    <ProductsList />
     </div>
   );
 }
---
+++ nextjs-shop/src/app/products.js
@@ -0,0 +1,14 @@
+const products = [
+    {
+        name: 'Semi-Sweet Chocolate Chip Cookie',
+        image: '/chocchip.jpg',
+        price: 1.500
+    },
+    {
+        name: 'Flavors of the Week',
+        image: '/box.jpg',
+        price: 10.000
+    }
+]
+
+export default products
